20915cae732da0d13cd43b7d2256b51d45849612
alioune <aliounek318@gmail.com>
2026-03-25 23:41:37 +0100

    fix: corregida ruta de la api a la carpeta poke-task

--- Docker/Caronte/dockerfiles/api/start-api.sh
+++ Docker/Caronte/dockerfiles/api/start-api.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Creamos la carpeta de logs para que no falle
 mkdir -p /root/logs
 LOG_FILE="/root/logs/informe-api.log"
 echo "=== INICIANDO API NESTJS ===" > $LOG_FILE
@@ -7,11 +6,11 @@ echo "=== INICIANDO API NESTJS ===" > $LOG_FILE
 # 1. Lanzamos la ciberseguridad heredada
 /usr/local/bin/start.sh &
 
-# 2. Entramos al código y preparamos Node
-cd /app
+# 2. Entramos a la subcarpeta real de tu código
+cd /app/poke-task
 echo "Instalando dependencias de la API..." >> $LOG_FILE
 npm install
 
-# 3. Lanzamos la API (Plan A, Plan B y Plan C)
+# 3. Lanzamos la API
 echo "Lanzando API..." >> $LOG_FILE
 npm run start:dev || npm run dev || npm start
